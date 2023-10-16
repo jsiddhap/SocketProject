ee88465e19111dd54557c6d2ca8f97e78057d3b4
ShraySoorma <84877829+ShraySoorma@users.noreply.github.com>
2023-10-15 20:45:37 -0700

    Update manager.py
    added functionality to end the game

--- manager.py
+++ manager.py
@@ -5,3 +5,6 @@ No, my partner is the best!
 shray soorma is mad and is crazy! 
 He went to dinner and now hes telling me to so git commits
 but issok
+
+
+my partner is pretty cool she set up the git repo and commented in it 
